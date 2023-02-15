cbfcd5c5ab3fae19b9814f9052cefe1b945ae58b
ZCwHmg <ZCwHmg@cyber-dojo.org>
2023-02-15 05:09:36 +0000

    6 ran tests, predicted green, got green

--- files/count_coins.js
+++ files/count_coins.js
@@ -1,7 +1,17 @@
 'use strict';
 
-function count_coins() {
-  return 1;
+function count_coins(cents, coinsList) {
+  coinsList.sort((a, b) => b - a);
+  const coinsForCents = new Array(cents + 1).fill(0);
+  coinsForCents[0] = 1;
+  coinsList.forEach(coin => {
+    coinsForCents.forEach((coinsForCent, index) => {
+      coinsForCents[index] += index < coin
+        ? 0
+        : coinsForCents[index - coin]
+    });
+  });
+  return coinsForCents.pop();
 }
 
 module.exports = count_coins;
