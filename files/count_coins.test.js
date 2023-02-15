a5fb509334c7699a8d24caffb9d9823431f2b8c4
ZCwHmg <ZCwHmg@cyber-dojo.org>
2023-02-15 04:21:07 +0000

    3 ran tests, predicted red, got red

--- files/count_coins.test.js
+++ files/count_coins.test.js
@@ -2,8 +2,14 @@
 
 const count_coins = require('./count_coins');
 
+const coinsList = [25, 10, 5, 1];
+
 describe('Count Coins', () => {
-  it('should return ', () => {
+  it('should return a number', () => {
     expect(typeof count_coins(1)).toEqual('number');
+  });
+
+  it('should return 1 for 1 cent', () => {
+    expect(count_coins(1)).toEqual(1);
   });
 });
